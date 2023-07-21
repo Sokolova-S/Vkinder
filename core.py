b8419789802ff39e1227f92e67c8d4e72e9f23af
Sokolova-S <131273459+Sokolova-S@users.noreply.github.com>
2023-07-21 22:21:46 +0700

    core.py

--- core.py
+++ core.py
@@ -131,7 +131,7 @@ class VkTools:
 
 
 if __name__ == '__main__':
-    user_id = 161842942
+    user_id = 
     tools = VkTools(access_token)
     params = tools.get_profile_info(user_id)
     worksheets = tools.search_worksheet(params, 20)
